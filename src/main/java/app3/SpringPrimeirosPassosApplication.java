651f692a90a6b2f43457624be660782c0e470719
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2025-02-24 16:08:00 -0300

    Properties Value

---
+++ src/main/java/app3/SpringPrimeirosPassosApplication.java
@@ -0,0 +1,14 @@
+package app3;
+
+import app3.SistemaMensagem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringPrimeirosPassosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(app.SpringPrimeirosPassosApplication.class, args);
+    }
+}
