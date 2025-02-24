66c7a25e875047cf61342b3cc464d91353d8c6a2
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2025-02-24 12:12:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Decola_Tech_2025
+ Bootcamp Avanade
---
+++ src/test/java/Decola_Tech_2025/DecolaTech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package Decola_Tech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
